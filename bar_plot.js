281c772d125252d2532b701e21aaacec6d4aec52
Saptarshi Ghose <saptarshighose@gmail.com>
2019-04-06 00:30:43 -0500

    Add files via upload

--- bar_plot.js
+++ bar_plot.js
@@ -63,10 +63,11 @@ svg_bar.selectAll("mybar")
 					   .attr("id", "tooltip")
 					   .attr("x", x_val_tooltip)
 					   .attr("y", y_val_tooltip)
+             .attr("dy", -20)
 					   .attr("text-anchor", "middle")
 					   .attr("font-size", "25px")
 					   .attr("fill", "black")
-					   .text(d);
+					   .text(d.State + " " + d.Value);
 			   })
 			   .on("mouseout", function() {
 					d3.select("#tooltip").remove();
@@ -86,93 +87,11 @@ var bar_pollution = svg_bar.append("text")
   .attr("dy", ".75em")
   .style("font-size", "23px")
   .text("US Cost of Electricity by State (2000)");
-
-//Function to Reveal 2017 Bar Chart
-function switch_data() {
-    //Set Margins
-    var margin_bar = {top: 20, right: 100, bottom: 100, left: 150},
-        width_bar = 1150 - margin.left - margin.right,
-        height_bar = 700 - margin.top - margin.bottom;
-
-    //SVG Bar Data
-    var svg_bar = d3.select("#bar_chart")
-      .append("svg")
-        .attr("width", width_bar + margin.left + margin.right)
-        .attr("height", height_bar + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.csv("electricity_cost.csv", function(error, data) {
-      var x_bar = d3.scaleBand()
-        .range([ 0, width_bar ])
-        .domain(data.map(function(d) { return d.State; }))
-        .padding(0.1);
-      svg_bar.append("g")
-        .attr("transform", "translate(0," + height_bar + ")")
-        .call(d3.axisBottom(x_bar))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
-
-      //Add Y Axis
-      var y_bar = d3.scaleLinear()
-        .domain([0, 18])
-        .range([ height_bar, 0]);
-      svg_bar.append("g")
-        .call(d3.axisLeft(y_bar));
-
-
-      //Add Y Axis label
-      svg_bar.append("text")
-      .attr("class", "y label")
-      .attr("text-anchor", "middle")
-      .attr("x", 70- (height_bar/ 2))
-      .attr("y", -70 )
-      .attr("transform", "rotate(-90)")
-      .style("text-decoration", "underline")
-      .text("Average Cost of Electricity (Cents per KW-Hour)");
-
-      // Add SVG Bars
-      svg_bar.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-          .attr("x", function(d) { return x_bar(d.State); })
-          .attr("y", function(d) { return y_bar(d.Value); })
-          .attr("width", x_bar.bandwidth())
-          .attr("height", function(d) { return height_bar - y_bar(d.Value); })
-          .attr("fill", "#FC8D62")
-          .on("mouseover", function(d) {
-                var x_val_tooltip = d3.select(this).attr("x");
-      					var y_val_tooltip = d3.select(this).attr("y");
-      					//Tooltip
-      					svg_bar.append("text")
-      					   .attr("id", "tooltip")
-      					   .attr("x", x_val_tooltip)
-      					   .attr("y", y_val_tooltip)
-      					   .attr("text-anchor", "middle")
-      					   .attr("font-size", "25px")
-      					   .attr("fill", "black")
-      					   .text(d);
-      			   })
-      			   .on("mouseout", function() {
-      					d3.select("#tooltip").remove();
-      			   });
-
-      //Add Source
-      svg_bar.append("text")
-        .text("Source: US Energy Information Administration")
-        .attr("x", width_bar/1.5 - 50)
-        .attr("y", height_bar+90 );
-
-      //Add Title
-      var bar_pollution = svg_bar.append("text")
-        .attr("x",width_pollution / 2)
-        .attr("text-anchor", "middle")
-        .attr("dy", ".75em")
-        .style("font-size", "23px")
-        .text("US Cost of Electricity by State (2017)");
-
-    });
-}
+//
+// d3.select(#update_button).on("click", function(){
+//   switch_data();
+// }
+//
+// d3.select(#update_button){
+//   .attr("class", "Disabled")
+// }
--- bar_plot_new.js
+++ bar_plot_new.js
@@ -1,4 +1,5 @@
 //Referenced: https://www.d3-graph-gallery.com/barplot.html, http://bl.ocks.org/d3noob/7030f35b72de721622b8
+
 //Set Margins
 var margin_bar_new = {top: 20, right: 100, bottom: 100, left: 150},
     width_bar_new = 1150 - margin.left - margin.right,
@@ -15,6 +16,7 @@ var svg_bar_new = d3.select("#bar_chart_new")
 
 //Load Data
 d3.csv("electricity_cost.csv", function(data) {
+
 //X Axis
 var x_bar_new = d3.scaleBand()
   .range([ 0, width_bar_new ])
@@ -33,7 +35,6 @@ var y_bar_new = d3.scaleLinear()
   .range([ height_bar_new, 0]);
 svg_bar_new.append("g")
   .call(d3.axisLeft(y_bar_new));
-
 
 //Add Y Axis label
 svg_bar_new.append("text")
@@ -81,8 +82,8 @@ svg_bar_new.append("text")
 })
 
 //Add Title
-var bar_pollution_new = svg_bar_new.append("text")
-  .attr("x",width_pollution_new / 2)
+var bar_pollution = svg_bar_new.append("text")
+  .attr("x",width_pollution / 2)
   .attr("text-anchor", "middle")
   .attr("dy", ".75em")
   .style("font-size", "23px")
